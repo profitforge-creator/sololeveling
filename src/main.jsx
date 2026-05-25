f1f7f9e59e85596fc4dff9ac5ab5b5b090a5f3cd
profitforge-creator <hesirq@gmail.com>
2026-05-25 18:37:42 -0400

    Initialize main.jsx with React and SystemInterface

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SystemInterface from './SystemInterface'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <SystemInterface />
+)
